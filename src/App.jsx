b644de49011f7043489d06bcc9e7b0b3c4230938
Ayesha <ayeshashaikh9c@gmail.com>
2026-08-12 20:36:56 +0530

    merged conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import Home from "./pages/home/home.jsx";
 
 import Portfolio from "./pages/portfolio/portfolio";
+
 function App() {
   return <Home />;
-}
-
-export default App;
   return <Portfolio />;
 }
 
 export default App;
+  
+
+
 
 
